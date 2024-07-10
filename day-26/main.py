462aa2d8bd4d09193aea051ed19907e747eaefb7
Luis Fernando Cardeña Ley <luis.fer160197@gmail.com>
2024-07-10 00:56:21 -0600

    Day 30. Add error handling to exercises in day 26 (NATO alphabet) and 29(Password manager)

--- day-26/main.py
+++ day-26/main.py
@@ -4,6 +4,14 @@ nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
 
-word = input("Enter a word: ").upper()
-nato_list = [nato_dict[letter] for letter in word]
-print(nato_list)
+nato_list = []
+
+while len(nato_list) == 0:
+    word = input("Enter a word: ").upper()
+    try:
+        nato_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_list = []
+    else:
+        print(nato_list)
--- day-29-pass-manager/main.py
+++ day-29-pass-manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -33,7 +34,12 @@ def save():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
-    data_line = f"{website} | {email} | {password}\n"
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Pleas make sure you haven't left any fields empty.")
@@ -41,10 +47,40 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered\nEmail: {email} "
                                                       f"\nPassword: {password}\n Is it ok to save?")
         if is_ok:
-            with open("vault.txt", "a") as file:
-                file.write(data_line)
+            try:
+                with open("vault.json", "r") as data_file:
+                    # Read old data
+                    json_data = json.load(data_file)
+            except FileNotFoundError:
+                with open("vault.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
+                # Update old data with new data
+                json_data.update(new_data)
+                with open("vault.json", "w") as data_file:
+                    # Save updated data
+                    json.dump(json_data, data_file, indent=4)
+            finally:
                 website_input.delete(0, END)
                 password_input.delete(0, END)
+
+# ------------------------- SEARCH WEBSITE ---------------------------- #
+
+def find_password():
+    website = website_input.get().lower()
+    try:
+        with open("vault.json", "r") as data_file:
+            json_data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        new_data = {item.lower():key for (item, key) in json_data.items()}
+        if website in new_data:
+            website_data = new_data[website]
+            messagebox.showinfo(title=website.title(), message=f"Email: {website_data["email"]}\n "
+                                f"\nPassword: {website_data["password"]}")
+        else:
+            messagebox.showinfo(title="Error", message="Website Not Found")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -61,8 +97,11 @@ website_lbl = Label(text="Website:")
 website_lbl.grid(column=0, row=1)
 
 website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2, sticky='EW')
+website_input.grid(column=1, row=1, sticky='EW')
 website_input.focus()
+
+website_search_btn = Button(text="Search", command=find_password)
+website_search_btn.grid(column=2, row=1, sticky='EW')
 
 email_lbl = Label(text="Email/Username:")
 email_lbl.grid(column=0, row=2)
